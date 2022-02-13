93ced3fc94bdf57af7e6556a0f8646aa7a8726be
wahyuadhiprabowo1 <042716384@ecampus.ut.ac.id>
2022-02-13 22:14:42 +0700

    Uas PPB

---
+++ app/src/main/java/studio/wahyu/uas_pbb/MainActivity3.kt
@@ -0,0 +1,12 @@
+package studio.wahyu.uas_pbb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity3 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setSupportActionBar(findViewById(R.id.toolbar))
+    }
+}
